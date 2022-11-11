e7fa3aee68f6b332dcd58bee6b67106bf224c00d
George Dawson <62862493+georgetdawson@users.noreply.github.com>
2022-11-11 00:01:18 +0000

    added gitignore and socket and threading for server

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import logging
 
 #Create and configure logger
 LOG_FORMAT = "%(Levelname)s %(asctime)s - %(message)s"
-logging.basicConfig(filename = "C:\\Users\George\Documents\coding\chat room python\logfile.log",
+logging.basicConfig(filename = "C:\\Users\George\Documents\coding\chat-room-python\logfile.log",
                     level = logging.DEBUG,
                     filemode = 'w')
                     
@@ -15,3 +15,8 @@ logger.warning("I'm sorry, but I can't do that, Dave.")
 logger.error("Did you just try to divide by zero?")
 logger.critical("The entire internet is down!!")
 
+# Importing socket and threading
+logger.debug("importing socket and threading")
+
+import socket
+import threading
